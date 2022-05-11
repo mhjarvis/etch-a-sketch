af53c3b570884932a80df90ad1ad72998d9a8eea
Markus <markus.h.jarvis@gmail.com>
2022-05-11 11:50:34 -0500

    Add loop to create 16x16 boxes

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log("Hello");
+
+const body = document.querySelector('body');
+const containerDiv = document.createElement('div');
+
+body.appendChild(containerDiv);
+containerDiv.setAttribute('class', 'container');
+
+const container = document.querySelector('.container');
+
+
+
+for(let i = 0; i < 16; i++) {
+
+    const div = document.createElement('div');
+    div.setAttribute('id', 'box-container' + i);
+    container.appendChild(div);
+
+    for(let j = 0; j < 16; j++) {
+
+        const boxContainer = document.querySelector('#box-container' + i);
+        const div = document.createElement('div');
+        div.setAttribute('class', 'box');
+        boxContainer.appendChild(div);
+    }
+}
